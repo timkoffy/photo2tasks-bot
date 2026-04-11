2743703400f772408c75f4469363138c3e84ab5e
timkoffy <nikitakirilltimur@gmail.com>
2026-04-11 11:51:23 +0400

    add: proxy

--- bot_instance.py
+++ bot_instance.py
@@ -1,8 +1,20 @@
 import telebot
 from dotenv import load_dotenv
 import os
+import requests
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+PROXY_URL = os.getenv("PROXY_URL")
+ORIGINAL_URL = "https://api.telegram.org"
+
+_old_request = requests.Session.request
+
+def _patched_request(self, method, url, *args, **kwargs):
+    if ORIGINAL_URL in url:
+        url = url.replace(ORIGINAL_URL, PROXY_URL)
+    return _old_request(self, method, url, *args, **kwargs)
+
+requests.Session.request = _patched_request
 
 bot = telebot.TeleBot(BOT_TOKEN)
